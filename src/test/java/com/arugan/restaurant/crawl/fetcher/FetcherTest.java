263345fbde161a28b65e553c3df60f3b573dde81
nagura4000 <nagura4000@gmail.com>
2014-12-22 21:36:25 +0900

    modified2

--- src/test/java/com/arugan/restaurant/crawl/fetcher/FetcherTest.java
+++ src/test/java/com/arugan/restaurant/crawl/fetcher/FetcherTest.java
@@ -3,9 +3,6 @@ package com.arugan.restaurant.crawl.fetcher;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
-import org.junit.Before;
-import org.junit.Test;
-
 import com.arugan.restaurant.crawl.link.LinkGetter;
 import com.arugan.restaurant.crawl.link.LinkGetterImp;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
@@ -17,21 +14,21 @@ public class FetcherTest {
 
 	private LinkGetter linkGetter;
 
-	@Before
-	public void init() {
-		fetcher = FetcherImp.getInstance();
-		linkGetter = new LinkGetterImp();
-	}
-
-	@Test
-	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
-		String url = "http://tabelog.com/washoku/kanagawa/A1405/0/0/COND-0-0-2-0-0-0-0-0/D-trend/40/";
-		HtmlPage page = fetcher.getPage(url);
-
-		for (String link :linkGetter.getLinkList(page)) {
-			System.out.println(link);
-		}
-	}
+//	@Before
+//	public void init() {
+//		fetcher = FetcherImp.getInstance();
+//		linkGetter = new LinkGetterImp();
+//	}
+//
+//	@Test
+//	public void test() throws FailingHttpStatusCodeException, MalformedURLException, IOException {
+//		String url = "http://tabelog.com/washoku/kanagawa/A1405/0/0/COND-0-0-2-0-0-0-0-0/D-trend/40/";
+//		HtmlPage page = fetcher.getPage(url);
+//
+//		for (String link :linkGetter.getLinkList(page)) {
+//			System.out.println(link);
+//		}
+//	}
 
 	private void start() {
 
