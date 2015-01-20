8590bcc22a9b123b58b1f225710781801efda593
nagura4000 <nagura4000@gmail.com>
2015-01-20 23:27:56 +0900

    編集

--- src/main/java/com/arugan/restaurant/crawl/fetcher/FetcherImp.java
+++ src/main/java/com/arugan/restaurant/crawl/fetcher/FetcherImp.java
@@ -5,7 +5,6 @@ import java.net.MalformedURLException;
 
 import org.apache.log4j.Logger;
 
-import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
@@ -19,9 +18,12 @@ public class FetcherImp implements Fetcher {
 	private Logger log = Logger.getLogger(this.getClass());
 
 	private FetcherImp() {
-		webClient = new WebClient(BrowserVersion.INTERNET_EXPLORER_9);
-		webClient.getOptions().setJavaScriptEnabled(false);
-		webClient.getOptions().setThrowExceptionOnScriptError(false);
+		webClient = new WebClient();
+		webClient.setJavaScriptEnabled(false);
+		webClient.setThrowExceptionOnScriptError(false);
+
+//		webClient.getOptions().setJavaScriptEnabled(false);
+//		webClient.getOptions().setThrowExceptionOnScriptError(false);
 
 	}
 
@@ -33,21 +35,23 @@ public class FetcherImp implements Fetcher {
 	}
 
 	public HtmlPage getPage(String url) {
-		HtmlPage page = null;
-		try {
-			log.info("target url:" + url);
-			page = webClient.getPage(url);
-			log.info("fetch OK.");
-		} catch (FailingHttpStatusCodeException e) {
-			log.error(e.getMessage());
-		} catch (MalformedURLException e) {
-			log.error(e.getMessage());
-		} catch (IOException e) {
-			log.error(e.getMessage());
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
-		return page;
+        System.out.println("target url=" + url);
+        HtmlPage page = null;
+        try {
+                log.info("target url:" + url);
+                page = webClient.getPage(url);
+                log.info("fetch OK.");
+        } catch (FailingHttpStatusCodeException e) {
+                log.error(e.getMessage());
+        } catch (MalformedURLException e) {
+                log.error(e.getMessage());
+        } catch (IOException e) {
+                log.error(e.getMessage());
+        } catch (Exception e) {
+                log.error(e.getMessage());
+        }
+        return page;
+
 	}
 
 }
